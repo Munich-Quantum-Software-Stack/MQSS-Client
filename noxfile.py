356989e6c0855d497eaeb9b9303af41c8a568ec5
Muhammad Farooqi <muhammad.farooqi@lrz.de>
2024-10-31 14:05:28 +0100

    fixed noxfile for python versions

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ import os
 os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+@nox.session()
 def test(session):
-    session.run("pdm", "install", external=True)
+    session.run("pdm", "install", "-G:all", external=True)
     session.run("pytest")
